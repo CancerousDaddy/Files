95e3ddd174add3a8037ef9e875e501c22cea2dd3
G0UTY <42919289+G0UTY@users.noreply.github.com>
2021-12-03 13:43:20 -0600

    Update and rename MainTheme.css to MainTheme.js

---
+++ MainTheme.js
@@ -0,0 +1,153 @@
+const converted = {
+  "#mw-content-container, .mw-footer-container": {
+    backgroundColor: "#00000000"
+  },
+  body: {
+    background:
+      "url(https://static.miraheze.org/supercubecavernwiki/c/c0/WoodTxture.png)",
+    backgroundRepeat: "repeat",
+    backgroundPosition: "center center",
+    backgroundSize: "100px"
+  },
+  "#mw-content-container": { borderBottom: "#00000000" },
+  ".mw-footer-container": {
+    boxShadow: "none",
+    borderTop: "#00000000",
+    margin: "0px 0"
+  },
+  "#mw-content": {
+    background:
+      'url("https://static.miraheze.org/supercubecavernwiki/3/32/BrickTxture.png") repeat',
+    backgroundPosition: "center center",
+    backgroundSize: "100px",
+    border: "solid 5px #ab8d5e"
+  },
+  ".color-middle, .color-left, .color-right": { background: "#52311c82" },
+  "#mw-header-container": {
+    background:
+      "url(https://static.miraheze.org/supercubecavernwiki/c/c0/WoodTxture.png)",
+    backgroundRepeat: "repeat",
+    backgroundPosition: "center center",
+    backgroundSize: "100px"
+  },
+  "#mw-site-navigation .sidebar-chunk, #mw-related-navigation .sidebar-chunk": {
+    background:
+      'url("https://static.miraheze.org/supercubecavernwiki/2/23/CarpetTxture.png") repeat',
+    backgroundPosition: "center center",
+    backgroundSize: "200px",
+    border: "solid 5px #9c2c2c",
+    color: "black"
+  },
+  "#mw-site-navigation .sidebar-chunk li, #mw-related-navigation .sidebar-chunk li a": {
+    color: "black"
+  },
+  ".mw-portlet-body li a": { color: "black" },
+  ".mw-body h1.firstHeading": { color: "black", fontWeight: "bold" },
+  ".mw-body h1.firstHeading, .tools-inline li": {
+    borderBottom: "solid 5px #ffffff87",
+    background: "#ffffff40",
+    textAlign: "center"
+  },
+  ".tools-inline li.selected": { borderBottom: "solid 5px #ffffff87" },
+  "#p-logo-text a.long": { color: "white" },
+  "#personal h2, #pt-notifications-alert .mw-echo-notifications-badge.mw-echo-notifications-badge-all-read, #pt-notifications-notice .mw-echo-notifications-badge.mw-echo-notifications-badge-all-read": {
+    filter: "invert()"
+  },
+  ".AboutGame": {
+    background:
+      "url(https://static.miraheze.org/supercubecavernwiki/c/c0/WoodTxture.png) repeat"
+  },
+  ".infobox": {
+    border: "1px solid #a2a9b1",
+    borderSpacing: "3px",
+    backgroundColor: "#f8f9fa",
+    color: "black",
+    margin: "0.5em 0 0.5em 1em",
+    padding: "0.2em",
+    cssFloat: "right",
+    clear: "right",
+    fontSize: "88%",
+    lineHeight: "1.5em",
+    width: "22em"
+  },
+  ".infobox-subbox": {
+    padding: "0",
+    border: "none",
+    margin: "-3px",
+    width: "auto",
+    minWidth: "100%",
+    fontSize: "100%",
+    clear: "none",
+    cssFloat: "none",
+    backgroundColor: "transparent"
+  },
+  ".infobox-header,\n.infobox-label,\n.infobox-above,\n.infobox-full-data,\n.infobox-data,\n.infobox-below,\n.infobox-subheader,\n.infobox-image,\n.infobox-navbar,\n\n.infobox th,\n.infobox td": {
+    verticalAlign: "top"
+  },
+  ".infobox-label,\n.infobox-data,\n\n.infobox th,\n.infobox td": {
+    textAlign: "left"
+  },
+  ".infobox .infobox-above,\n.infobox .infobox-title,\n\n.infobox caption": {
+    fontSize: "125%",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  ".infobox-title,\n\n.infobox caption": { padding: "0.2em" },
+  ".infobox .infobox-header,\n.infobox .infobox-subheader,\n.infobox .infobox-image,\n.infobox .infobox-full-data,\n.infobox .infobox-below": {
+    textAlign: "center"
+  },
+  ".infobox .infobox-navbar": { textAlign: "right" },
+  ".infobox.bordered": { borderCollapse: "collapse" },
+  ".infobox.bordered td,\n.infobox.bordered th": {
+    border: "1px solid #a2a9b1"
+  },
+  ".infobox.bordered .borderless td,\n.infobox.bordered .borderless th": {
+    border: "0"
+  },
+  ".infobox.bordered .mergedtoprow td,\n.infobox.bordered .mergedtoprow th": {
+    border: "0",
+    borderTop: "1px solid #a2a9b1",
+    borderRight: "1px solid #a2a9b1"
+  },
+  ".infobox.bordered .mergedrow td,\n.infobox.bordered .mergedrow th": {
+    border: "0",
+    borderRight: "1px solid #a2a9b1"
+  },
+  ".infobox.geography": {
+    borderCollapse: "collapse",
+    lineHeight: "1.2em",
+    fontSize: "90%"
+  },
+  ".infobox.geography  td,\n.infobox.geography  th": {
+    borderTop: "1px solid #a2a9b1",
+    padding: "0.4em 0.6em 0.4em 0.6em"
+  },
+  ".infobox.geography .mergedtoprow td,\n.infobox.geography .mergedtoprow th": {
+    borderTop: "1px solid #a2a9b1",
+    padding: "0.4em 0.6em 0.2em 0.6em"
+  },
+  ".infobox.geography .mergedrow td,\n.infobox.geography .mergedrow th": {
+    border: "0",
+    padding: "0 0.6em 0.2em 0.6em"
+  },
+  ".infobox.geography .mergedbottomrow td,\n.infobox.geography .mergedbottomrow th": {
+    borderTop: "0",
+    borderBottom: "1px solid #a2a9b1",
+    padding: "0 0.6em 0.4em 0.6em"
+  },
+  ".infobox.geography .maptable td,\n.infobox.geography .maptable th": {
+    border: "0",
+    padding: "0"
+  },
+  ".wikitable.plainrowheaders th[scope=row]": {
+    fontWeight: "normal",
+    textAlign: "left"
+  },
+  ".wikitable td ul,\n.wikitable td ol,\n.wikitable td dl": {
+    textAlign: "left"
+  },
+  ".toc.hlist ul,\n#toc.hlist ul,\n.wikitable.hlist td ul,\n.wikitable.hlist td ol,\n.wikitable.hlist td dl": {
+    textAlign: "inherit"
+  },
+  "table.mw-hiero-table td": { verticalAlign: "middle" }
+}
